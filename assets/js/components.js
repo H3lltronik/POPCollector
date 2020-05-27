6432e51a63f35dd61f0db75dd65e922d819700b2
H3lltronik <esau.egs1@gmail.com>
2020-05-26 21:35:14 -0500

    Componente del search

--- assets/js/components.js
+++ assets/js/components.js
@@ -6,6 +6,7 @@ import ProductForm from './components/productForm.vue'
 import SeBuscaForm from './components/seBuscaForm.vue'
 import RecommendForm from './components/recommendForm.vue'
 import PersonalizationForm from './components/personalizationForm.vue'
+import SearchBox from './components/searchBox.vue'
 
 Vue.use(ElementUI);
 
@@ -19,6 +20,7 @@ new Vue ({
         'se-busca-form-component': SeBuscaForm,
         'recommend-form-component': RecommendForm,
         'personalization-form-component': PersonalizationForm,
+        'search-component': SearchBox,
     },
     created() {
     }
---
+++ assets/js/components/searchBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <form action="/product/all" method="GET" class="toolbar_search">
+        <div class="row mx-0">
+            <el-input class="w-100" name="searchParam" placeholder="Buscar..." v-model="form.busqueda">
+                <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            currentcategory: {
+                default: ""
+            },
+            form: {},
+        }
+    },
+    computed: {
+        searchPlaceholder () {
+            if (this.currentcategory) {
+                return "Buscar en " + JSON.stringify(this.currentcategory)
+            } else {
+                return "Buscar"
+            }
+        }
+    }
+}
+</script>
